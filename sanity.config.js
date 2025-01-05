ea42653bf1ba268919a35b19e4ecd83be48aec26
ebox86 <ebox86@gmail.com>
2025-01-05 03:30:21 -0500

    cleaned up sanity config

--- sanity.config.js
+++ sanity.config.js
@@ -3,13 +3,10 @@ import { defineConfig } from "sanity";
 import { deskTool } from 'sanity/desk'
 import { defaultDocumentNode } from "./src/components/previewIFrame";
 import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
-import { scheduledPublishing } from "@sanity/scheduled-publishing";
 import { contentGraphView } from "sanity-plugin-graph-view";
 import {media} from 'sanity-plugin-media'
 import schemas from "./schemas/schema";
 import deskStructure from "./deskStructure";
-import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
-import vars from 'env.local.json'
 
 export default defineConfig({
   title: "BMW CCA PSR Website",
@@ -22,20 +19,7 @@ export default defineConfig({
     }),
     unsplashImageAsset(),
     contentGraphView({}),
-    //scheduledPublishing(),
-    media(),
-    netlifyWidget({
-      title: 'My Netlify deploys',
-      sites: [
-        {
-          title: 'Sanity Studio',
-          apiId: vars.apiId,
-          buildHookId: vars.buildHookId,
-          name: 'bmw-club-psr',
-          url: 'https://bmw-club-psr.org'
-        },
-      ]
-    })
+    media()
   ],
   schema: {
     types: schemas,
